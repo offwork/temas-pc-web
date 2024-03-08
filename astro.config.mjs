c4bd723a2276c1da564fbc13b6a311413d798575
offwork <chomunist@gmail.com>
2024-03-09 01:07:22 +0300

    github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://offwork.github.io',
+  base: '/temas-pc-web',
   integrations: [tailwind(), icon({
     include: {
       "heroicons": ["*"]
     }
   })],
-  output: "server",
-  adapter: netlify()
 });
